dcbb707e80e35caf011114ec0428c1460a9b5ef8
Daniel Segovia <segoarce90@gmail.com>
2025-04-21 18:04:41 -0700

    type output functions

--- zod-v3.ts
+++ zod-v3.ts
@@ -1,14 +1,18 @@
 import type z from "zod";
 import { MutableRedacted, Redacted } from "./mod.ts";
 
-export const redactedZodV3 = <T>(zodType: z.ZodType<T>) => {
+export const redactedZodV3 = <T>(
+  zodType: z.ZodType<T>,
+): z.ZodEffects<z.ZodType<T, z.ZodTypeDef, T>, Redacted<T>, T> => {
   return zodType.transform<Redacted<T>>((value) => {
     zodType.parse(value);
     return new Redacted(value);
   });
 };
 
-export const mutableRedactedZodV3 = <T>(zodType: z.ZodType<T>) => {
+export const mutableRedactedZodV3 = <T>(
+  zodType: z.ZodType<T>,
+): z.ZodEffects<z.ZodType<T, z.ZodTypeDef, T>, MutableRedacted<T>, T> => {
   return zodType.transform<MutableRedacted<T>>((value) => {
     zodType.parse(value);
     return new MutableRedacted(value);
